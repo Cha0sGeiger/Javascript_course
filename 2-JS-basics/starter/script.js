adbb7961b7cfa22f7bdba647b765a0e19340c3c6
Cha0sGeiger <33580180+Cha0sGeiger@users.noreply.github.com>
2019-08-16 15:24:19 +0200

    Add files via upload

---
+++ 2-JS-basics/starter/script.js
@@ -0,0 +1,46 @@
+/********************************** 
+ * Variables and data types
+*/
+
+/*
+
+var firstName = 'John';
+var lastName = 'Smith';
+var age = 29;
+console.log(firstName,lastName,age);
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = 'Teacher';
+console.log(job);
+
+*/
+
+//Variable can start with _ or $ or letter
+
+
+/********************************** 
+ * Variables mutation and type coercion
+*/
+
+var firstName = 'John';
+var age = 28;
+
+console.log(firstName + ' '+ age)
+
+var job, isMarried;
+
+job = 'teacher';
+isMarried = 'false';
+
+console.log(firstName + ' ' + age + ' year old ' + job + '. Is he married ? ' + isMarried);
+
+
+
+
+
+
